6babede82e9b1dc31913e004568ecdc09fb01c11
Taras <19Zefir@gmail.com>
2021-08-20 19:11:36 +0300

    Add tours and header fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , HostListener} from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    header_scroll:boolean = false;
+    
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-
-
+    ngOnInit(): void {
+    }
+    @HostListener("document:scroll")
+    scrollfunction(){
+        if(scrollY>5){
+            this.header_scroll = true;
+        }
+        else{
+            this.header_scroll = false;
+        }
+    }
+    
+    
+   
 }
